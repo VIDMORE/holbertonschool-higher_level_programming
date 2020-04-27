e216d75160e59104b56ce758efacb919728a1fa1
David Corredor <vidmore8@gmail.com>
2020-04-27 16:17:39 -0500

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle
+ * @list: list to examinate
+ * Return: 1 if true, 0 if false
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *current;
+
+	head = list;
+	current = list;
+
+	while (head)
+	{
+		while (current)
+		{
+			if (head == current->next)
+				return (1);
+
+			current = current->next;
+		}
+		head = head->next;
+	}
+	return (0);
+}
