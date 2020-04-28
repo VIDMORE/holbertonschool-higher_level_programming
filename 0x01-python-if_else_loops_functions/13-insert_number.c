c25b69ac714572ca24ed641e9849a19cacfaef2b
David Corredor <vidmore8@gmail.com>
2020-04-28 16:23:26 -0500

    [Upload] Uploading task #13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,73 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts an integer in a determinated node
+ * @head: head of the list
+ * @number: number to add
+ * Return: List
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temporal = *head;
+	unsigned int i = 0;
+
+	while (temporal)
+	{
+		if (temporal->n < number && temporal->next->n > number)
+		{
+			i++;
+			break;
+		}
+
+		i++;
+		temporal = temporal->next;
+	}
+
+	return (insert_nodeint_at_index(head, i, number));
+}
+
+/**
+ * insert_nodeint_at_index - inset a node in the given head
+ * @head: head of the list
+ * @idx: index
+ * @n: number to set
+ * Return: return the new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new, *temporal;
+
+	temporal = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (temporal != NULL && i < (idx - 1))
+	{
+		temporal = temporal->next;
+		i++;
+	}
+
+	if (temporal == NULL)
+	{
+		return (NULL);
+	}
+
+	new->next = temporal->next;
+	temporal->next = new;
+
+	return (new);
+}
