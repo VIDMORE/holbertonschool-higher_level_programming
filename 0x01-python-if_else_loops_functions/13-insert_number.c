1111648df9283e141d6fe2955c3bed612ff7aff3
David Corredor <vidmore8@gmail.com>
2020-04-28 20:46:05 -0500

    [Fix] Fix in task #13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,68 +9,38 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *temporal = *head;
-	unsigned int i = 0, flag = 0;
-
-	while (temporal)
-	{
-		if (temporal->n < number && temporal->next->n > number)
-		{
-			i++;
-			flag = 1;
-			break;
-		}
-
-		i++;
-		temporal = temporal->next;
-	}
-	if (flag == 1)
-		return (insert_nodeint_at_index(head, i, number));
-
-	return (*head);
-}
-
-/**
- * insert_nodeint_at_index - inset a node in the given head
- * @head: head of the list
- * @idx: index
- * @n: number to set
- * Return: return the new node
- */
-
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	unsigned int i = 0;
-	listint_t *new, *temporal;
-
-	temporal = *head;
-
+	listint_t *temp = *head;
+	listint_t *new;
+	
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
 
-	new->n = n;
+	new->n = number;
+	new->next = NULL;
 
-	if (idx == 0)
+	if (*head == NULL)
 	{
-		new->next = *head;
 		*head = new;
 		return (new);
 	}
 
-	while (temporal != NULL && i < (idx - 1))
+	while (temp != NULL)
 	{
-		temporal = temporal->next;
-		i++;
+		if (temp->next->n > number)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			break;
+		}
+		if (temp->next == NULL)
+		{
+			temp->next = new;
+			break;
+		}
+		temp = temp->next;
 	}
-
-	if (temporal == NULL)
-	{
-		return (NULL);
-	}
-
-	new->next = temporal->next;
-	temporal->next = new;
 
 	return (new);
 }
