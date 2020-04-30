12fec7ca30b9cb198fd10d21f61a32488346035d
David Corredor <vidmore8@gmail.com>
2020-04-30 12:09:41 -0500

    [Fix] Fixing issues in task #1 and #2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
+if __name__ == "__main__":
+    count = len(argv)
 
-count = len(argv)
-
-if count == 1:
-    print("0 arguments.")
-elif count == 2:
-    print("1 argument:")
-    print("1: {:s}".format(argv[1]))
-else:
-    print("{} arguments:".format(count - 1))
-    for i in range(1, count):
-        print("{:d}: {:s}".format(i, argv[i]))
+    if count == 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    else:
+        print("{} arguments:".format(count - 1))
+        for i in range(1, count):
+            print("{:d}: {:s}".format(i, argv[i]))
