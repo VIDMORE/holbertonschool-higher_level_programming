c9653d9030a110ac20a15cd0f3ce7fc95cd2b621
David Corredor <vidmore8@gmail.com>
2020-05-05 18:06:51 -0500

    [Upload] - task #10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
     new_l = []
     new_l = [True if value % 2 == 0 else False for value in my_list]
     return new_l
