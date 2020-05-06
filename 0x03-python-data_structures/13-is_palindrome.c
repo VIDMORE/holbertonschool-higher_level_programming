b6b82ddef39a7f19652cd7a19fc425bebef17e4d
David Corredor <vidmore8@gmail.com>
2020-05-05 21:15:50 -0500

    [Upload] - task #13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,69 +8,32 @@
 
 int is_palindrome(listint_t **head)
 {
-	int counter = 0;
-	dlistint_t *new, *copy;
-
-	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	if (*head == NULL)
 		return (1);
 
-	while ((*head))
-	{
-		add_dnodeint_end(&new, (*head)->n);
-		(*head) = (*head)->next;
-		counter++;
-	}
-	copy = new;
-
-	while (copy->next)
-		copy = copy->next;
-
-	while (new)
-	{
-		if (copy->n == new->n)
-		{
-			copy = copy->prev;
-			new = new->next;
-		}
-		else
-			return (0);
-	}
-	return (1);
+	return (equal(head, *head));
 }
 
 /**
- * add_dnodeint_end - function that adds a node at the end
- * @head: head of the list
- * @n: number of the instance
- * Return: new list
+ * is_palindrome - is_palindrome
+ * @head: head
+ * Return: return
  */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+int equal(listint_t **first, listint_t *last)
 {
-	dlistint_t *new, *temp;
+	if (!last)
+		return (1);
 
-	temp = *head;
-
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	if (!*head)
+	if (equal(first, last->next))
 	{
-		new->prev = NULL;
-		*head = new;
-	}
-	else
-	{
-		while (temp->next)
-			temp = temp->next;
-
-		new->prev = temp;
-		temp->next = new;
+		
+		if ((*first)->n == last->n)
+		{
+			*first = (*first)->next;
+			return (1);
+		}
 	}
 
-	return (new);
+	return (0);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,26 +19,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * for Holberton project
- */
-typedef struct dlistint_s
-{
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
-} dlistint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+int equal(listint_t **first, listint_t *lasts);
 
 #endif /* MAGO_H */
