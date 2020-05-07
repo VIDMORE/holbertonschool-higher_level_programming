9669c62b49093ac36dc49028594ad760f57d0635
David Corredor <vidmore8@gmail.com>
2020-05-07 14:44:11 -0500

    [Upload] - task #10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary.iterkeys(), key=lambda k: a_dictionary[k])
+        return max(a_dictionary, key=lambda k: a_dictionary[k])
     else:
         return None
