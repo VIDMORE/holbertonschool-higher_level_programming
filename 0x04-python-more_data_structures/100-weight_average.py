12ce8b8cfa99d20838780397f597223ff0d050ff
David Corredor <vidmore8@gmail.com>
2020-05-07 20:21:08 -0500

    [Fix] - task #13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,10 @@
 def weight_average(my_list=[]):
     su = 0
     di = 0
+    result = 0
     if my_list:
         for values in my_list:
             su += values[0] * values[1]
             di += values[1]
-    return su / di
+        result = su / di
+    return result
