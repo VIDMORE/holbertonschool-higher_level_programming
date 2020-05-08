435067a49d7d531ad5b3452196ee6f0fb16470cb
David Corredor <vidmore8@gmail.com>
2020-05-07 19:33:04 -0500

    [Upload] - task #12

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new_list = list(a_dictionary.keys())
+    for val in new_list:
+        if value is a_dictionary[val]:
+           a_dictionary.pop(val)
+    return a_dictionary
