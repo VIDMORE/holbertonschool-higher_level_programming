600ec183549c91aaeb3344da84c34945d236abb5
David Corredor <vidmore8@gmail.com>
2020-05-18 13:14:55 -0500

    [Fix] - New exception added

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as te:
+    except (ValueError, TypeError) as te:
         print("Exception:", te, file=stderr)
         return False
     return True
