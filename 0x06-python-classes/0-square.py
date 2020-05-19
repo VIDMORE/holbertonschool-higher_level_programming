2e7e35c38af16fff06fea98f55409a4a83a2d3db
David Corredor <vidmore8@gmail.com>
2020-05-19 12:27:57 -0500

    [Upload] - Tasks 0 - 5

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module creates a new class Square"""
+
+
+class Square:
+    """ Empty Square classs """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a class Square
+    With a constuctor method
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        self.__size = size
