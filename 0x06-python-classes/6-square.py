ad97d6e1cdb0f6791971604af4361a6492887cdd
David Corredor <vidmore8@gmail.com>
2020-05-19 21:08:09 -0500

    [Fix] - Adding validations in contructor

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-       if type(position) is not tuple or len(position) != 2:
+        if type(position) is not tuple or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if (type(position[0]) is not int or type(position[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -97,7 +97,7 @@ class Square:
         Args:
             value: value for __position attribute
         """
-         if type(value) is not tuple or len(value) != 2:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if (type(value[0]) is not int or type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
