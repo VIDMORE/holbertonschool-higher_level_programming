77cb47c0b5c4c5f15a4e70bfe7692b771a71083e
David Corredor <vidmore8@gmail.com>
2020-05-19 22:52:10 -0500

    [Upload] - task #103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ class MagicClass:
     def __init__(self, radius=0):
         self.__radius = 0
 
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
 
         self.__radius = radius
@@ -15,3 +15,5 @@ class MagicClass:
 
     def circumference(self):
         return ((2 * math.pi) * self.__radius)
+
+ma = MagicClass(2)
