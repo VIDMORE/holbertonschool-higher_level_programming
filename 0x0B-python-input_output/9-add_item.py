eb476348aada7cae2ee6a63784bd23ba7ccbd4b1
David Corredor <vidmore8@gmail.com>
2020-06-03 17:15:06 -0500

    [FIX] - addign main function

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,20 +3,22 @@
 
 
 from sys import argv
-from os import path
+import os.path
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+if __name__ == '__main__':
+    new_list = []
+    file_name = "add_item.json"
 
-new_list = []
-file_name = "add_item.json"
+    # Creating file if doesn't exist and adding the list
+    if not os.path.exists('add_item.json'):
+        save_to_json_file(new_list, file_name)
 
-if not path.exists(file_name):
+    # Bringing the list from the file
+    new_list = load_from_json_file(file_name)
+
+    for i in range(1, len(argv)):
+        new_list.append(str(argv[i]))
+
     save_to_json_file(new_list, file_name)
-
-new_list = load_from_json_file(file_name)
-
-for i in range(1, len(argv)):
-    new_list.append(str(argv[i]))
-
-save_to_json_file(new_list, file_name)
