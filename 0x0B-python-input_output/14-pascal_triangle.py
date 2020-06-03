4a0ee7d8555ee1efb255e598b09a871f1218d4be
David Corredor <vidmore8@gmail.com>
2020-06-03 18:10:33 -0500

    [UPLOAD] - Recursive triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
-
+"""This module defines the functions for the pascal triangle"""
 
 def pascal_triangle(n):
+    """
+    Gets each row and appends to the triangle
 
-    counter = 1
-    triangle = [[1]]
-    row = []
-    for i in range(1, n):
-        row = [1]
-        for i in range(i - 1):
-            row.append(i + counter)
-        counter += 1
-        row.append(1)
-        triangle.append(row)
+    Args:
+        n times
+
+    Returns:
+        triangle
+    """
+
+    triangle = []
+    for i in range(n):
+        triangle.append(rows(i))
     return triangle
+
+
+def rows(n):
+    """
+    Gets each row
+
+    Args:
+        n times
+
+    Returns:
+        row to append
+    """
+
+    litt = []
+    if n == 0:
+        return [1]
+    else:
+        row = rows(n-1)
+        litt.append(1)
+        litt += [row[i] + row[i+1] for i in range(n-1)]
+        litt.append(1)
+
+        return litt
