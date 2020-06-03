2b6d9cf6cc1004de0342661c5c9ca236f135b594
David Corredor <vidmore8@gmail.com>
2020-06-03 17:18:44 -0500

    [FIX] - changing argv

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,18 +7,18 @@ import os.path
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-if __name__ == '__main__':
-    new_list = []
-    file_name = "add_item.json"
 
-    # Creating file if doesn't exist and adding the list
-    if not os.path.exists('add_item.json'):
-        save_to_json_file(new_list, file_name)
 
-    # Bringing the list from the file
-    new_list = load_from_json_file(file_name)
+new_list = []
+file_name = "add_item.json"
 
-    for i in range(1, len(argv)):
-        new_list.append(str(argv[i]))
-
+# Creating file if doesn't exist and adding the list
+if not os.path.exists(file_name):
     save_to_json_file(new_list, file_name)
+
+# Bringing the list from the file
+new_list = load_from_json_file(file_name)
+
+new_list.append(argv[1:])
+
+save_to_json_file(new_list, file_name)
