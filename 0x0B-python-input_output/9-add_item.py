8076e1d87ac6db2906db26672e6ab2fac7f31375
David Corredor <vidmore8@gmail.com>
2020-06-03 17:08:30 -0500

    [FIX] - new_list eliminated

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,15 +8,14 @@ import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-new_list = []
 file_name = "add_item.json"
 
 if not os.path.exists(file_name):
-    save_to_json_file(new_list, file_name)
+    save_to_json_file([], file_name)
 
 new_list = load_from_json_file(file_name)
 
 for i in range(1, len(argv)):
-    new_list.append(argv[i])
+    new_list.append(str(argv[i]))
 
 save_to_json_file(new_list, file_name)
