28dfb3926677a6a642ac89d5a33a574c64674adb
David Corredor <vidmore8@gmail.com>
2020-06-03 17:09:58 -0500

    [FIX] - change import

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,7 +3,7 @@
 
 
 from sys import argv
-import os import path
+from os import path
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
