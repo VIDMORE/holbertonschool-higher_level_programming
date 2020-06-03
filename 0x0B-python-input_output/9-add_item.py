6a4872ab2e10c0f86943a9061b26f3c696506137
David Corredor <vidmore8@gmail.com>
2020-06-03 18:39:42 -0500

    [NFI] - Adding fu**** permissions

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -11,10 +11,12 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 file_name = "add_item.json"
 args = argv[1:]
-new_list = []
 
-# Creating file if doesn't exist and adding the list
-if os.path.exists(file_name):
-    new_list = load_from_json_file(file_name)
+if not os.path.exists(file_name):
+    save_to_json_file([], file_name)
 
-save_to_json_file(new_list + args, file_name)
+new_list = load_from_json_file(file_name)
+
+new_list.append(args)
+
+save_to_json_file(new_list, file_name)
