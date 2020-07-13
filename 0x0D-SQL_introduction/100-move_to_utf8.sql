4b18bc673367e175c423b11a82d97724e2eb71d7
David Corredor <vidmore8@gmail.com>
2020-07-13 15:53:24 -0500

    [UPLOAD] - Task #17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Printing all Dbs in my workspace
+-- because mysql the best!
+
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
