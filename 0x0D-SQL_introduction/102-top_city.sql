f48559e03f84052d032e86240204b2404fce5bdc
David Corredor <vidmore8@gmail.com>
2020-07-13 16:41:07 -0500

    [FIX] - Adding the between

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
 -- Printing all Dbs in my workspace
 -- because mysql the best!
 
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
