b008c78da2b6188fef8990b5889c888238e2407d
David Corredor <vidmore8@gmail.com>
2020-07-14 22:07:50 -0500

    [FIX] - Task #0 and Task 7

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Printing all Dbs in my workspace
 -- because mysql the best!
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
