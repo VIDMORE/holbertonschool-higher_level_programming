a31d4bc32b5baebc4a767fe839bee45ff9a9a060
David Corredor <vidmore8@gmail.com>
2020-08-18 19:20:40 -0500

    [UPLOAD] - CACHAMA POWER

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,6 @@ def find_peak(list_of_integers):
     """
     This function returns the peak of a list
     """
-    if list_of_integers == []:
-        return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    if list_of_integers != []:
+        list_of_integers.sort()
+        return list_of_integers[-1]
