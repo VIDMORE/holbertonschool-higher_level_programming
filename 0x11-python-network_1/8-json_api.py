b35c181b0253f32e68e621b8554e0da7686700d5
David Corredor <vidmore8@gmail.com>
2020-08-24 16:23:28 -0500

    [UPLOAD] - Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,15 @@
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    q = argv[1] if len(argv) > 1 else ""
+    try:
+        req = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+        req = req.json()
+        if 'id' in req and 'name' in req:
+            print("[{}] {}".format(req['id'], req['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
