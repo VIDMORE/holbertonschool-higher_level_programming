203e9801aab361ead2e5b0e4cb0b2de6902146f8
David Corredor <vidmore8@gmail.com>
2020-09-15 14:29:07 -0500

    [UPLOAD] - Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function secondBiggest(array) {
+function secondBiggest (array) {
   if (array.length < 4) {
-    return 0
+    return 0;
   }
-  const data = array.sort()
-  return data[data.length - 2]
+  const data = array.sort();
+  return data[data.length - 2];
 }
 console.log(secondBiggest(process.argv));
