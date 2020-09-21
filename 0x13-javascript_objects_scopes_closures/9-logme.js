a24b4b65572e68679c73c589b781441cda797108
David Corredor <vidmore8@gmail.com>
2020-09-21 12:25:41 -0500

    [FIX] - Changing logic using enclosure

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 exports.logMe = (function () {
   let counter = 0;
-  return (item) => { console.log(`${counter}: ${item}`); counter++;};
+  return (item) => { console.log(`${counter}: ${item}`); counter++; };
 }());
